be58a77141e19d293195f190f75fd1807b07e99a
Emily <emi.roca18@gmail.com>
2021-02-25 20:05:36 -0400

    Se creo el controlador Calculator y se crearon las operaciones principales y la pruba correspondiente con Post (que corrio bien)

---
+++ solcalculadora/apicalculadora/Controllers/CalculatorController.cs
@@ -0,0 +1,35 @@
+﻿namespace apicalculadora.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+       [HttpPost]
+        [Route("Add")]
+        public int Add ([FromHeader] int numa, [FromHeader] int numb)
+        {
+            return numa + numb;
+        }
+        [HttpPost]
+        [Route("Add2")]
+        public int Add2 ([FromHeader] int numa, [FromHeader] int numb)
+        {
+            return numa * numb;
+        }
+        [HttpPost]
+        [Route("Add3")]
+        public int Add3 ([FromHeader] int numa, [FromHeader] int numb)
+        {
+            return numa - numb;
+        }
+        [HttpPost]
+        [Route("Add4")]
+        public int Add4 ([FromHeader] int numa, [FromHeader] int numb)
+        {
+            return numa / numb;
+        }
+
+    }
+}
