b5c030fa064c4b73c78645d8bf9473731475e2fc
Emily <emi.roca18@gmail.com>
2021-02-27 10:01:56 -0400

    se agrego get a suma,resta,multiplicacion y division

--- solcalculadora/apicalculadora/Controllers/CalculatorController.cs
+++ solcalculadora/apicalculadora/Controllers/CalculatorController.cs
@@ -12,22 +12,57 @@
         {
             return numa + numb;
         }
+        [HttpGet]
+        [Route("suma")]
+        public int suma2 (int numa, int numb)
+        {
+            return numa + numb;
+        }
+
         [HttpPost]
         [Route("multiplicacion")]
         public int multiplicacion ([FromHeader] int numa, [FromHeader] int numb)
         {
             return numa * numb;
         }
+        [HttpGet]
+        [Route("multiplicacion")]
+        public int multiplicacion2 (int numa,int numb)
+        {
+            return numa * numb;
+        }
+
         [HttpPost]
         [Route("resta")]
         public int resta ([FromHeader] int numa, [FromHeader] int numb)
         {
             return numa - numb;
         }
+
+        [HttpGet]
+        [Route("resta")]
+        public int resta2(int numa, int numb)
+        {
+            return numa - numb;
+        }
+
         [HttpPost]
         [Route("division")]
         public int division ([FromHeader] int numa, [FromHeader] int numb)
         {
+            if (numb == 0)
+            return 0;
+
+            return numa / numb;
+        }
+
+        [HttpGet]
+        [Route("division")]
+        public int division2 (int numa, int numb)
+        {
+            if (numb == 0)
+                return 0;
+
             return numa / numb;
         }
 
